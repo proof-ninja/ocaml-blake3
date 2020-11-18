e05eac14760cc5304f9ac1af3e15146ce9d1f0af
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2020-11-18 11:54:19 +0900

    fix the pretreatment

--- src/blake3.ml
+++ src/blake3.ml
@@ -2,13 +2,9 @@ let bytes_of_array a =
   let len = Array.length a in
   String.init len (fun i -> char_of_int @@ Array.get a i)
 
-let array_of_bytes s =
-  let len = String.length s in
-  Array.init len (fun i -> int_of_char @@ String.get s i)
-
-external hash_rust: int array -> int array = "blake3_hash"
+external hash_rust: string -> int array = "blake3_hash"
 
 let hash s =
-  array_of_bytes s
+  s
   |> hash_rust
   |> bytes_of_array
